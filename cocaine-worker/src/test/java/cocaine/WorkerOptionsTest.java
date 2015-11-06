7eea0e9699f559e1abc39e4cf258b08556cdab03
Alexander Kirakozov <akirakozov@yandex-team.ru>
2015-11-06 10:10:39 +0300

    Update options test

--- cocaine-worker/src/test/java/cocaine/WorkerOptionsTest.java
+++ cocaine-worker/src/test/java/cocaine/WorkerOptionsTest.java
@@ -14,14 +14,22 @@ public class WorkerOptionsTest {
     @Test
     public void parse() {
         WorkerOptions options = new WorkerOptions();
-        new JCommander(options, "--app", "example", "--endpoint", "/tmp/cocaine.sock",
-                "--uuid", "26e82218-161f-11e4-be84-1867b0bd9c14", "-d", "120000", "-h", "12000");
+        new JCommander(options,
+                "--app", "example",
+                "--endpoint", "/tmp/cocaine.sock",
+                "--uuid", "26e82218-161f-11e4-be84-1867b0bd9c14",
+                "--locator", "locator",
+                "--protocol", "1",
+                "-d", "120000",
+                "-h", "12000");
 
         Assert.assertEquals("example", options.getApplication());
         Assert.assertEquals("/tmp/cocaine.sock", options.getEndpoint());
         Assert.assertEquals(UUID.fromString("26e82218-161f-11e4-be84-1867b0bd9c14"), options.getUuid());
         Assert.assertEquals(120000, options.getDisownTimeout());
         Assert.assertEquals(12000, options.getHeartbeatTimeout());
+        Assert.assertEquals("locator", options.getLocator());
+        Assert.assertEquals("1", options.getProtocol());
     }
 
 }
