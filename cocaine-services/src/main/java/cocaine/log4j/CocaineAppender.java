a959b62d063da70998656d89a35bd812c264d388
Alexander Kirakozov <akirakozov@yandex-team.ru>
2016-01-28 20:40:52 +0300

    One sourceName for cocaine appender (it should be app name)

--- cocaine-services/src/main/java/cocaine/log4j/CocaineAppender.java
+++ cocaine-services/src/main/java/cocaine/log4j/CocaineAppender.java
@@ -10,15 +10,17 @@ import org.apache.log4j.spi.LoggingEvent;
 public class CocaineAppender extends AppenderSkeleton {
 
     private final Logging delegate;
+    private final String sourceName;
 
-    public CocaineAppender(Logging delegate) {
+    public CocaineAppender(String sourceName, Logging delegate) {
         this.delegate = delegate;
+        this.sourceName = sourceName;
     }
 
     @Override
     protected void append(LoggingEvent event) {
         String message = getLayout().format(event);
-        delegate.append(event.getLevel(), event.getLoggerName(), message);
+        delegate.append(event.getLevel(), sourceName, message);
     }
 
     @Override
