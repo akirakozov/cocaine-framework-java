0a464bfc46a9964b4294f3390381ba5d1abb12f7
Vladimir Polyakov <vap.polyakov@gmail.com>
2017-02-21 19:24:18 +0300

    add unistorage read2 support without response headers and xml

--- cocaine-services/src/main/java/cocaine/Unistorage.java
+++ cocaine-services/src/main/java/cocaine/Unistorage.java
@@ -5,6 +5,8 @@ import cocaine.annotations.CocaineService;
 import cocaine.session.Session;
 import cocaine.unistorage.StorageResponse;
 
+import java.util.Map;
+
 /**
  * @author metal
  */
@@ -12,4 +14,7 @@ import cocaine.unistorage.StorageResponse;
 public interface Unistorage extends AutoCloseable {
     @CocaineMethod("read")
     Session<StorageResponse> read(String ns, String key, long coupleId, long offset, long size, long chunkSize);
+
+    @CocaineMethod("read2")
+    Session<StorageResponse> read2(String key, long offset, long size, long chunkSize, Map<String, String> params);
 }
