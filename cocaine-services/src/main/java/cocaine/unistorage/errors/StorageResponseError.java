52edb1a62cd9ec3b309464c99a2d3271ed9f8047
Dmitriy Amelin <lemeh@yandex-team.ru>
2017-06-08 10:13:48 +0300

    add error type as field to StorageResponseError

--- cocaine-services/src/main/java/cocaine/unistorage/errors/StorageResponseError.java
+++ cocaine-services/src/main/java/cocaine/unistorage/errors/StorageResponseError.java
@@ -6,7 +6,10 @@ import cocaine.ServiceException;
  * @author metal
  */
 public class StorageResponseError extends ServiceException {
+    public final StorageErrorType type;
+
     public StorageResponseError(String service, StorageErrorType type, String message) {
         super(service, type.toString() + ": " + message);
+        this.type = type;
     }
 }
